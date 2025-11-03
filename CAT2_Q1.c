ff87d43be5f833ccd244d0a97bf8ad3fb03c21fc
Kiptui Kipyator PA106/G/28740/25 <kiptuizaccpi@gmail.com>
2025-11-03 23:21:45 +0300

    Add files via upload

---
+++ CAT2_Q1.c
@@ -0,0 +1,24 @@
+/*
+NAME: Kiptui Kipyator 
+REG NO: PA106/G/28740/25
+Date: 3rd November 2025
+*/
+
+#include <stdio.h>
+
+int main() {
+    int scores[2][4] = {
+        {65, 92, 35, 70},
+        {84, 72, 59, 67}
+    };
+    
+    // Printing elements
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("%d ", scores[i][j]);
+        }
+        printf("\n"); // move to next row
+    }
+    
+    return 0;
+}
